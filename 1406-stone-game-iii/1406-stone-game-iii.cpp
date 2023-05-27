114404e5a42d68f0ba2efed50770257cfc83eec0
Aneesh Khunteta <96608925+ANEESH-2003@users.noreply.github.com>
2023-05-27 15:44:10 +0530

    Time: 284 ms (43.13%), Space: 137.6 MB (43.60%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -5,33 +5,26 @@ public:
     {
         if(i==n)
             return 0;
-        if(dp[i]!=-1 && c==0)
+        if(dp[i]!=-1)
             return dp[i];
         int maxi=INT_MIN;
-        if(c)
-            maxi=INT_MAX;
         int s=0;
         for(int j=0;i+j<n && j<=2;j++)
         {
             s+=v[i+j];
             int a=cal(v,i+j+1,1-c,dp);
-            if(c==0)
-                maxi=max(maxi,s+a);
-            else
-                maxi=min(maxi,a);
+            maxi=max(maxi,s-a);
         }
-        if(c==0)
-            dp[i]=maxi;
-        return maxi;
+        return dp[i]=maxi;    
     }
     string stoneGameIII(vector<int>& v) {
         n=v.size();
         int s=accumulate(v.begin(),v.end(),0);
         vector<int> dp(n,-1);
         int c=cal(v,0,0,dp);
-        if(s-c<c)
+        if(0<c)
             return "Alice";
-        else if(s-c>c)
+        else if(0>c)
             return "Bob";
         else
             return "Tie";
