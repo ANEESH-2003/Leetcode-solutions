bf9bb02fff868358bea033253ec013f9ce9a0412
Aneesh Khunteta <96608925+ANEESH-2003@users.noreply.github.com>
2023-06-02 14:15:17 +0530

    Time: 129 ms (70.66%), Space: 18.4 MB (44.19%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool cal(vector<int>& a,vector<int>& b)
+    {
+        return (pow((a[0]-b[0]),2)+pow((a[1]-b[1]),2)<=pow((a[2]),2));
+    }
+    int fun(vector<vector<int>>& v,vector<int>& vis,int i)
+    {
+        vis[i]++;
+        int c=1;
+        for(auto j:v[i])
+        {
+            if(vis[j])
+                continue;
+            c+=fun(v,vis,j);
+        }
+        return c;
+    }
+    int maximumDetonation(vector<vector<int>>& b) {
+        int n=b.size();
+        int maxi=0;
+        vector<vector<int>> v(n);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(cal(b[i],b[j]))
+                    v[i].push_back(j);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            vector<int> vis(n,0);
+            maxi=max(maxi,fun(v,vis,i));
+        }
+        return maxi;
+    }
+};
