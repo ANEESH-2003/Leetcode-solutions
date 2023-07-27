f181c6d59fe505f19056d7ef8ba37791d6318a7d
Aneesh Khunteta <96608925+ANEESH-2003@users.noreply.github.com>
2023-07-27 14:40:41 +0530

    Time: 204 ms (11.41%), Space: 45.2 MB (33.38%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -14,8 +14,26 @@ public:
         
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        int ans=cal(coins,0,amount,dp);
-        return ans>1e4?-1:ans;
+        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,0));
+        int n=coins.size();
+        for(int i=0;i<n;i++)
+        {
+            if(amount>=coins[i])
+            dp[i][coins[i]]=1;
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                int take=1e4+1,not_take=1e4+1;
+                if(coins[i]<=j)
+                    take=1+dp[i][j-coins[i]];
+                if(i)
+                    not_take=dp[i-1][j];
+                dp[i][j]=min(take,not_take);
+            }
+        }
+        return dp[n-1][amount]>1e4?-1:dp[n-1][amount];
+        //return ans>1e4?-1:ans;
     }
 };
