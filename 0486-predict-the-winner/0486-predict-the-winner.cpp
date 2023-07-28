d40c0a1f697d449c38355d81070958ad7f909368
Aneesh Khunteta <96608925+ANEESH-2003@users.noreply.github.com>
2023-07-28 10:05:13 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (29.30%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    int cal(vector<int>& nums,int l,int r,int p)
+    int cal(vector<int>& nums,int l,int r,int p,vector<vector<int>>& dp)
     {
         if(l>r)
             return 0;
-        int first=p*nums[l]+cal(nums,l+1,r,-p);
-        int last=p*nums[r]+cal(nums,l,r-1,-p);
-        return p==-1?min(first,last):max(first,last);
+        if(dp[l][r]!=1e9)
+            return dp[l][r];
+        int first=p*nums[l]+cal(nums,l+1,r,-p,dp);
+        int last=p*nums[r]+cal(nums,l,r-1,-p,dp);
+        return dp[l][r]=p==-1?min(first,last):max(first,last);
     }
     bool PredictTheWinner(vector<int>& nums) {
-        return cal(nums,0,nums.size()-1,1)>=0;
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),1e9));
+        return cal(nums,0,nums.size()-1,1,dp)>=0;
     }
 };
