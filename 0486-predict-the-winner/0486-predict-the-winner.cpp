c8f455c7ec922e75ed54309bd3eeec99766d6499
Aneesh Khunteta <96608925+ANEESH-2003@users.noreply.github.com>
2023-07-28 11:04:05 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (29.30%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -11,7 +11,23 @@ public:
         return dp[l][r]=p==-1?min(first,last):max(first,last);
     }
     bool PredictTheWinner(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),1e9));
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),0));
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            dp[i][i]=(n%2?1:-1)*nums[i];
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int p=(n-1-j+i+1)%2?1:-1;
+                int first=p*nums[i]+dp[i+1][j];
+                int last=p*nums[j]+dp[i][j-1];
+                dp[i][j]=p==-1?min(first,last):max(first,last);
+            }
+        }
+        return dp[0][n-1]>=0;
         return cal(nums,0,nums.size()-1,1,dp)>=0;
     }
 };
