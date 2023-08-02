e66e91e69892bbcc5ee5640c8fbb8012f5efa572
Aneesh Khunteta <96608925+ANEESH-2003@users.noreply.github.com>
2023-08-02 17:27:08 +0530

    Time: 6 ms (58.58%), Space: 6.5 MB (76.28%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -16,7 +16,35 @@ public:
     bool isInterleave(string a, string b, string c) {
         if(a.size()+b.size()!=c.size())
             return 0;
-        vector<vector<int>> v(a.size()+1,vector<int>(b.size()+1,-1));
-        return cal(a,b,c,0,0,v);
+        vector<vector<bool>> v(a.size()+1,vector<bool>(b.size()+1,0));
+        for(int i=0;i<=a.size();i++)
+        {
+            for(int j=0;j<=b.size();j++)
+            {
+                if(i==0 && j==0)
+                {
+                    v[i][j]=true;
+                }
+                else if(i==0)
+                {
+                    if(b[j-1]==c[i+j-1])
+                        v[i][j]=v[i][j]||v[i][j-1];
+                }
+                else if(j==0)
+                {
+                    if(a[i-1]==c[i+j-1])
+                        v[i][j]=v[i][j]||v[i-1][j];
+                }
+                else
+                {
+                    if(a[i-1]==c[i+j-1])
+                        v[i][j]=v[i][j]||v[i-1][j];
+                    if(b[j-1]==c[i+j-1])
+                        v[i][j]=v[i][j]||v[i][j-1];
+                }
+            }
+        }
+        return v[a.size()][b.size()];
+        //return cal(a,b,c,0,0,v);
     }
 };
