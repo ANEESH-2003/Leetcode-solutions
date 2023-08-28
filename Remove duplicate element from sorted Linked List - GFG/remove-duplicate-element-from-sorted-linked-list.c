b688f26bc1cb575e398a9d84a9595146300f38e1
Aneesh Khunteta <96608925+ANEESH-2003@users.noreply.github.com>
2023-08-28 15:53:22 +0530

    Added solution - LeetHub

---
+++ Remove duplicate element from sorted Linked List - GFG/remove-duplicate-element-from-sorted-linked-list.c
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+//Initial Template for C
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+}*start;
+
+void insert();
+void display(struct Node *head);
+
+
+// } Driver Code Ends
+//User function Template for C
+
+void removeDuplicates(struct Node* head)
+{
+    struct Node* cur=head->next;
+    struct Node* prev=head;
+    while(cur)
+    {
+        if(cur->data==prev->data)
+        {
+            prev->next=cur->next;
+            cur=cur->next;
+        }
+        else
+        {
+            prev=cur;
+            cur=cur->next;
+        }
+    }
+    return head;
+}
+
+
+
+//{ Driver Code Starts.
+
+    
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+      start=NULL;
+      insert();
+      removeDuplicates(start);
+      display(start);
+      printf("\n");
+    }
+    return 0;
+
+}
+
+
+ void insert()
+ {
+     int n,value,i;
+     scanf("%d",&n);
+     struct Node *temp;
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&value);
+         if(i==0)
+         {
+              start=(struct Node *) malloc( sizeof(struct Node) );
+              start->data=value;
+              start->next=NULL;
+              temp=start;
+              continue;
+         }
+         else
+         {
+             temp->next= (struct Node *) malloc( sizeof(struct Node) );
+             temp=temp->next;
+             temp->data=value;
+             temp->next=NULL;
+         }
+     }
+ }
+ 
+ void display(struct Node *head)
+{
+   while(head!=NULL)
+  {
+   printf("%d ",head->data);
+    head=head->next;
+  }
+}
+
+
+
+
+
+// } Driver Code Ends
